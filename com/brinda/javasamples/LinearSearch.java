8cabe79860a84521709e974ec5781d13054a8c58
SivakumarParthiban <pvpsiva@gmail.com>
2023-08-27 07:45:19 -0400

    Linear search

---
+++ com/brinda/javasamples/LinearSearch.java
@@ -0,0 +1,29 @@
+package com.brinda.javasamples;
+
+public class LinearSearch {
+ static int i;
+
+	public static void main(String[] args) {
+		
+		
+	int a[]= {10,20,30,40,50};
+	
+	int search_else=10;
+	
+	for(int i=0;i<a.length;i++);
+	{
+        
+	
+		// System.out.println(a[i]);
+	}
+		if(search_else==a[i])
+		{
+			System.out.println("Element found at: "+i);
+		}
+			else
+			
+		System.out.println("Element not found");
+	}
+
+	
+}
